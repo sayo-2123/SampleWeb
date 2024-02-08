5e4da7d024337fdd4905c312ac75a0ce9ed0e762
sahara <sahara@DESKTOP-GF35UAK>
2024-02-08 15:03:11 +0900

    ログイン可否による画面遷移

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.LoginForm;
 
+import lombok.experimental.var;
+
 
 @Controller
 public class LoginController {
+	
+//	ログイン照合のための仮
+	private static final String LOGIN_ID ="user";
+	private static final String PASSWORD ="pwd";
 	
 	@GetMapping("/login")
 	public String view(Model model, LoginForm form) {
@@ -17,7 +23,13 @@ public class LoginController {
 		return "login";
 	}
 	@PostMapping("/login")
-	public void login(LoginForm form) {
-		System.out.println(form.toString());
+	public String login(Model model,LoginForm form) {
+		var isCorrectUserAuth= form.getLoginId().equals(LOGIN_ID) && form.getPassword().equals(PASSWORD);
+		if (isCorrectUserAuth) {
+			return "redirect:/menu";
+		} else {
+			model.addAttribute("errorMsg", "ログインIDとパスワードの組み合わせが間違っています。");
+			return "login";
+		}
 	}
 }
