7fbeac0a2b773df07f8200914513a6227d609dcb
sahara <sahara@DESKTOP-GF35UAK>
2024-02-08 14:38:28 +0900

    loginformの実装　ID password受け取りまで

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,23 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.form.LoginForm;
+
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String view() {
+	public String view(Model model, LoginForm form) {
+		
 		return "login";
+	}
+	@PostMapping("/login")
+	public void login(LoginForm form) {
+		System.out.println(form.toString());
 	}
 }
