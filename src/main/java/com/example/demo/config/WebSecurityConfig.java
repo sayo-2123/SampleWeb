752a8f5cb55d9d3791739872e24702da2a083211
sayo-2123 <classa1990@gmail.com>
2024-02-21 13:41:20 +0900

    デフォルトエラーメッセージの変更

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,12 +1,19 @@
 package com.example.demo.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.constant.UrlConst;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Spring Securityカスタマイズ用
@@ -14,9 +21,19 @@ import com.example.demo.constant.UrlConst;
  */
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 	
+	/** パスワードエンコーダー */
+	private final PasswordEncoder passwordEncoder;
+
+	/** ユーザー情報取得Service */
+	private final UserDetailsService userDetailsService;
+
+	/** メッセージ取得 */
+	private final MessageSource messageSource;
 	/** ユーザー名のname属性 */
+	
 	private final String USERNAME_PARAMETER = "loginId";
 
 	/**
@@ -40,5 +57,19 @@ public class WebSecurityConfig {
 
 
 		return http.build();
+	}
+	/**
+	 * Provider定義
+	 * 
+	 * @return カスタマイズProvider情報
+	 */
+	@Bean
+	AuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setMessageSource(messageSource);
+
+		return provider;
 	}
 }
